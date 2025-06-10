ebddcfd9c5be637d00b3754dcbe1bcce61920034
fvp1233 <fernadomiguelvelasquezperez@gmail.com>
2025-06-10 15:15:24 -0600

    CRUD completo

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ function mostrarDatos(datos){
     const tabla = document.querySelector('#tabla tbody');
     tabla.innerHTML = ''; //vaciamos el contenido de la tabla
 
+    //En el boton editar se colocan comillas simples si es una cadena de caracteres
     datos.forEach(persona => {
         tabla.innerHTML += `
         <tr>
@@ -29,7 +30,7 @@ function mostrarDatos(datos){
             <td>${persona.email}</td>
             <td>${persona.edad}</td>
                 <td>
-                    <button>Editar</button>
+                    <button onclick="abrirModalEditar(${persona.id}, '${persona.nombre}', '${persona.apellido}','${persona.email}','${persona.edad}' )">Editar</button>
                     <button onClick="EliminarRegistro(${persona.id})">Eliminar</button>
                 </td>        
         </tr>
@@ -108,3 +109,54 @@ async function EliminarRegistro(id){
     }
 
 }
+
+//Proceso para editar un registro
+ const modalEditar = document.getElementById("modal-editar");
+ const btnCerrarEditar = document.getElementById("btnCerrarEditar");
+
+btnCerrarEditar.addEventListener("click" , () =>{
+    modalEditar.close(); //cerrar modal
+});
+
+function abrirModalEditar(id,nombre, apellido, email, edad){
+    //Se agregan los valores del registro en los input
+    document.getElementById("idEditar").value = id;
+    document.getElementById("nombreEditar").value = nombre;
+    document.getElementById("apellidoEditar").value = apellido;
+    document.getElementById("emailEditar").value = email;
+    document.getElementById("edadEditar").value = edad;
+    
+    //Modal se abre después de agregar los valores a los input
+    modalEditar.showModal(); 
+}
+
+document.getElementById("frmEditar").addEventListener("submit", async e => {
+    e.preventDefault(); //Evita que el formulario se envie
+    
+    const id = document.getElementById("idEditar").value;
+    const nombre = document.getElementById("nombreEditar").value.trim();
+    const apellido = document.getElementById("apellidoEditar").value.trim();
+    const email = document.getElementById("emailEditar").value.trim();
+    const edad = document.getElementById("edadEditar").value.trim();
+
+    if(!id|| !nombre || !apellido || !email || !edad){
+        alert("Complete todos los campos");
+        return; //Evita que el código se siga ejecutando
+    }
+
+    //Llamada a la API
+    const respuesta = await fetch(`${API_URL}/${id}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({edad, email, nombre, apellido})
+    });
+
+    if(respuesta.ok){
+        alert("Registro Actualizado con éxito");//Confirmación
+        modalEditar.close();//Cerramos el modal
+        obtenerPersonas();//Actualizamos la lista
+    }
+    else{
+        alert("Hubo un error al actualizar");
+    }
+});
